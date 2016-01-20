27a2019143b94ce980381a292ffa61a4d1e762e9
D. Erik Goodman <goodman.d.erik@gmail.com>
2016-01-20 16:22:55 -0700

    Merge PC updates

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -3,46 +3,57 @@ using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
 
-	int max = 1000;
-	int min = 1;
-	int guess = 500;
-	
 	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	
 	void Start () {
-		print ("Welcome to Number Wizard.");
-		print ("Pick a number in your head, but don't tell me!");
+		StartGame ();
+	}
+
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = 500;
 		
-		
+		print ("======New Game=======");
+		print ("Welcome To Number Wizard");
+		print ("Pick a number in your head, but don't tell me what it is.");
+	
 		print ("The highest number you can pick is " + max);
-		max += 1;
 		print ("The lowest number you can pick is " + min);
-		
+	
 		print ("Is the number higher or lower than " + guess + "?");
-		print ("Up = higher, Down = lower, return = equal.");
-		
+		print ("Press the UP arrow for higher, the DOWN arrow for lower, or enter for equals.");
+		max++ ;
+	}
+	
+	
+	void NextGuess (){
+		guess = (max + min) / 2;
+		print ("Higher or lower than " + guess + "?");
+		print ("Press the UP arrow for higher, the DOWN arrow for lower, or enter for equals.");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (max < guess || min > guess ){
 			print ("No one likes a liar...");
-		}
-		
-		else if (Input.GetKeyDown(KeyCode.UpArrow)){
+			
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			//print ("Up Arrow pressed");
 			min = guess + 1;
-			guess = (max + guess) / 2;
-			print ("Is the number higher or lower than " + guess + "?");
-		}
-		
-		else if (Input.GetKeyDown(KeyCode.DownArrow)){
+			NextGuess();
+			
+		}else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			//print ("Down Arrow pressed");
 			max = guess;
-			guess = (min + guess) / 2;
-			print ("Is the number higher or lower than " + guess + "?");
-		}
-		
-		else if (Input.GetKeyDown(KeyCode.Return)){
+			NextGuess();
+			
+		}else if (Input.GetKeyDown (KeyCode.Return)) {
 			print ("I won!");
+			StartGame ();
 		}
-		
 	}
 }
